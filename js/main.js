38095b82cee9f8623202538f35a5be4bb1ffd466
andrewBar-ko <andrewbarkovets@gmail.com>
2021-03-24 08:30:22 +0300

    feat(main.js): feat opening and closing cartModal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+'use strict';
+
+// Slider
 const mySwiper = new Swiper('.swiper-container', {
 	loop: true,
 
@@ -8,3 +11,55 @@ const mySwiper = new Swiper('.swiper-container', {
 	},
 });
 
+// Cart
+const btnCart = document.querySelector('.button-cart'),
+	overlay = document.querySelector('.overlay'),
+	modalCart = document.querySelector('#modal-cart'),
+	modalClose = document.querySelector('.modal-close');
+
+const openModal = () => {
+
+	modalCart.classList.add('show');
+
+};
+
+const closeModal = () => {
+
+	modalCart.classList.remove('show');
+
+};
+
+// Scroll smooth function All
+{
+	const scrollLinks = document.querySelectorAll('a.scroll-link');
+
+	for (let i = 0; i < scrollLinks.length; i++) {
+
+		const sclink = e => {
+			e.preventDefault();
+			const id = scrollLinks[i].getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		};
+
+		scrollLinks[i].addEventListener('click', sclink);
+
+	}
+}
+
+// closing the Modal by Esc
+document.addEventListener('keydown', e => {
+
+	if (e.code === 'Escape') {
+		closeModal();
+	}
+
+});
+
+btnCart.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+
+// closing the Modal by click overlay
+overlay.addEventListener('click', closeModal);
